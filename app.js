f6eef02218d6e0b5800c90ea9605f740a16eddec
Sully Brooks <sullybrooks@icloud.com>
2019-06-24 17:05:29 -0500

    DB Connection Setup...needs 2 be fixed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const db = require('./db');
 
 var app = express();
 
@@ -29,6 +30,11 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+db.connect(() => {
+  app.listen(process.env.PORT || 5555, function (){
+    console.log(`Listening`);
+  });
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -40,13 +46,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var db = require('./db');
 
-db.connect(() => {
-  app.listen(process.env.PORT || 5555, function (){
-    console.log(`Listening`);
-  });
-});
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const db = require('./db');
+const db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("At Home");
+  db.get().collection('Users').find({}).toArray().then((users => {
+    console.log('Users', users);
+  }));
   res.render('index', { title: 'Express' });
 });
 
