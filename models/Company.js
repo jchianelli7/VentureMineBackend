68fae97d4d3147d5edc5baa81027139cb7c625da
Sully Brooks <sullybrooks@icloud.com>
2019-08-20 14:00:11 -0500

    AuctionIDs used rather than actual auction schema objects

--- models/Company.js
+++ models/Company.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
-var AuctionSchema = require('./Auction');
 var UserSchema = require('./User');
 
 var CompanySchema = new mongoose.Schema({
     companyName: {type: String, required: true},
-    auctions: {type: [AuctionSchema], required: true, default: []},
+    auctionIds: {type: [String], required: true, default: []},
     generalBio: {type: String, required: true, default: "Default Bio"},
     images: {type: [String], required: true, default: []},
     staff: {type: [{}], required: true, default: []},
