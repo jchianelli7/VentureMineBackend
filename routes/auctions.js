80da659f2b94e3c9bcf21a63904ce08c9f46adbc
Sully Brooks <sullybrooks@icloud.com>
2019-08-20 09:14:05 -0500

    .

--- routes/auctions.js
+++ routes/auctions.js
@@ -55,6 +55,7 @@ io.on("connect", socket => {
                     }
                     if (auction) {
                         socket.join(auction.id);
+                        console.log("Joined Room: ", auction.id);
                         auction.currentStrikePrice = this.getStrikePrice(auction);
                         auction.reserveMet = auction.currentStrikePrice >= auction.reservePrice;
 
@@ -85,10 +86,12 @@ io.on("connect", socket => {
                         auction.volumeData = volData;
                         auction.save(function(err, savedAuction) {
                             if(err){
-                                console.log("Error Saving Auction");
+                                console.log("Error Saving Auction", err);
                             }else{
-                                socket.emit('bidPlaced', savedAuction);
+                                // socket.emit('bidPlaced', savedAuction);
+                                io.in(bidData.auctionId).emit('bidPlaced', savedAuction);
                             }
+
                         })
                     }
                 });
